9c0d441c584d42d6c47450c6dc02309b505d48bd
vsuttle <vince.suttle@btinternet.com>
2022-11-18 13:12:53 +0000

    Used a stream to generate all the values in the fionacci sequence up to the nth element.

--- recursion/fibonacci/FibonacciStream.java
+++ recursion/fibonacci/FibonacciStream.java
@@ -1,2 +1,40 @@
-package recursion;public class FibonacciStream {
+import java.util.stream.IntStream;
+
+/**
+ * This time we generate all the fibonacci sequence numbers instead of just the nth one.
+ * This time we will make use of the streams library.
+ */
+public class FibonacciStream {
+
+  // The first two values in the sequence.
+  // Local variables must be final in a lambda expression
+  private static int previous = 0, next = 1, temp = 0;
+
+  public static void main(String[] args) {
+    generateFibSequence(3);
+    generateFibSequence(5);
+    generateFibSequence(10);
+  }
+
+  private static void generateFibSequence(int n) {
+    System.out.println("\nn: " + n);
+    streamSequence().limit(n).forEachOrdered(System.out::println);
+    reset();
+  }
+
+  private static void reset() {
+    previous = 0;
+    next = 1;
+    temp = 0;
+  }
+
+  private static IntStream streamSequence() {
+    return IntStream.generate(() -> {
+      temp = previous;
+      previous = next;
+      next += temp;
+      return temp;
+    });
+  }
+
 }
